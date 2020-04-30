024f00be6dd51f7c47ff5a022590eb630beae1e4
bcolety <61987823+ben-colety@users.noreply.github.com>
2020-04-30 10:15:39 +0200

    Update kibble_sim.m

--- kibble_sim.m
+++ kibble_sim.m
@@ -1,4 +1,4 @@
-clear all; clc;
+
 %CONCEPTION DES MECANISMES II
 %BALANCE DE KIBBLE PROJET 2020
 %GROUP 11 SOLUTION 1
@@ -19,48 +19,52 @@ clear all; clc;
 %   beta    : angle between vertical bar and vertical
 %   x       : horizontal displacment of end effector from origin
 %   z       : vertical displacement of end effector from origin
+clear all; clc;
 
 %% materials initialization
 safety_factor = 2;
 alu = struct('E', 69e9, 'o_adm', 110e6/safety_factor);
 
-materials = [alu];
+Materials = [alu];
 
-%% position initialization
+%% Position Initialization
 syms alpha beta z
 gamma = alpha + beta;
 
-
-%% watt linkage dimensions
-L1 = 200e-3;            % length of horizontal bar
-L2 = 200e-3;            % length of vertical bar
+%% Watt Linkage Dimensions
+L1 = 150e-3;            % length of horizontal bar
+L2 = 75e-3;             % length of vertical bar
 L3 = 250e-3;            % vertical distance between watt linkages
-L4 = 300e-3;            % vertical distance between center of watt linkages
+L4 = 300e-3;            % vertical distance between center of "S"
                         % and end effector
 checkDims(L1, L2, L3, L4);
 
 x_shift = 1e-6;
 
-%% compensation dimensions
+%% Compensation Dimensions
+    %mass
+    m = 3;
+    g = 9.81;
     %chariot
-    Lc1 = 1;        % natural length of spring - initial compressed length
-    Lc2 = 1;        % length of lames for parallel leaf spring
-    Lc3 = 1;        % length of linking lame
+    Lc1 = 200e-3;       % distance spring is compressed when z = 0
+    Lc2 = 30e-3;        % length of lames for parallel leaf spring
+    Lc3 = 100e-3;        % length of linking lame
 
     char_disp = Lc3 - sqrt(Lc3^2 - z^2);
     
     %weight
+    Lg = m*g/1000;  %extension of spring at z = 0 for k = 1000
     
 %% Pivots/Springs
 %watt linkages
     p1 = struct('location', 'gnd-a1',   ...
                 'type',     'cross',    ...
-                'k',        4,          ...
-                'cor_adm',  5,          ...
+                'k',        400,        ...     %effective rigidity
+                'cor_adm',  5,          ...     %course admissible
                 'ener_var', alpha       );      %variable linked to energy
     p2 = struct('location', 'a1-a2',    ...
                 'type',     'cross',    ...
-                'k',        4,          ...
+                'k',        300,          ...
                 'cor_adm',  5,          ...
                 'ener_var', gamma       );
     p3 = p2;    p3.location = 'a2-a3';
@@ -71,7 +75,7 @@ x_shift = 1e-6;
     p8 = p1;    p8.location = 'b3-gnd';
     p9 = struct('location', 'a2-S',     ...
                 'type',     'cross',    ...
-                'k',        4,          ...
+                'k',        1000,          ...
                 'cor_adm',  5,          ...
                 'ener_var', beta        );
     p10 = p9;   p10.location = 'b2-s';
@@ -79,17 +83,17 @@ x_shift = 1e-6;
 %chariots
     sc = struct('location', 'gnd-c1',       ...
                 'type',     'comp_spring',  ...
-                'k',        4,              ...
+                'k',        8000,           ...
                 'cor_adm',  1000,           ... %N/A
-                'ener_var', char_disp       );
+                'ener_var', Lc1-char_disp);
     pc1 = struct('location', 'gnd-c1',      ...
                 'type',     'parallel',     ...
-                'k',        4,              ...
+                'k',        500,              ...
                 'cor_adm',  5,              ...
                 'ener_var', char_disp       );
     pc2 = struct('location', 'c1-c2',       ...
                 'type',     'col',          ...
-                'k',        4,              ...
+                'k',        1000,              ...
                 'cor_adm',  5,              ...
                 'ener_var', asin(z/Lc3)     );
     pc3 = pc2; pc3.location = 'c2-s';
@@ -97,30 +101,77 @@ x_shift = 1e-6;
 %weight comp
     sg = struct('location', 'gnd-s',        ...
                 'type',     'spring',       ...
-                'k',        4,              ...
+                'k',        1000,           ...
                 'cor_adm',  1000,           ... %N/A
-                'ener_var', z               );
+                'ener_var', z+Lg            );
             
-%% simulation
+%lever
+    
+    
+            
+Pivots_link = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10];
+Pivots_chariot = [sc, pc1, pc2, pc3];
+Pivots_weight = [sg];
+Pivots = [Pivots_link, Pivots_chariot, Pivots_weight];
+            
+%% Simulation
+
+%initialization
 alpha_pas = 0.001;
-alpha(1) = 0;
-z(1) = 0;
-x(1) = 0;
-beta(1) = 0;
+alpha = zeros(500, 1);
+beta = zeros(500, 1);
+x = zeros(500, 1);
+z = zeros(500, 1);
+beta = zeros(500, 1);
 i = 1;
-while abs(x(i)) < 1e-6
-    plot(alpha(i), z(i), 'rx');hold on
+%linkage descending
+while (abs(x(i)) < 1e-6 && abs(z(i)) < 15e-3) || abs(z(i)) < 30e-3
     i=i+1;
     alpha(i) = alpha(i-1)+ alpha_pas;
     [beta(i), x(i), z(i)] = motionSim(alpha(i), L1, L2);
 end
-alpha(i) = 0;
-z(i) = 0;
-x(i) = 0;
-beta(i) = 0;
+%reset to 0
+i=i+1;
+%rising
+while (abs(x(i)) < 1e-6 && abs(z(i)) < 15e-3) || abs(z(i)) < 30e-3
+    i=i+1;
+    alpha(i) = alpha(i-1)- alpha_pas;
+    [beta(i), x(i), z(i)] = motionSim(alpha(i), L1, L2);
+end
 
+%trimming arrays
+alpha = alpha(2:find(alpha,1,'last'));
+beta = beta(2:find(beta,1,'last'));
+x = x(2:find(x,1,'last'));
+z = z(2:find(z,1,'last'));
+
+%sorting arrays
+[alpha, sort_ind] = sort(alpha);
+beta = beta(sort_ind);
+z = z(sort_ind);
+x = x(sort_ind);
+
+%% Energy & Force
+Energies = zeros(length(z), length(Pivots));
+for i = 1:length(Pivots)
+    ener = calcEnergy(Pivots(i));
+    Energies(:,i) = eval(ener);
+end
+sumEnergies = sum(Energies, 2);
+
+%Force
+sampling_res = max(z)*2/(length(z)*4);
+sampling_pts = min(z):sampling_res:max(z);
+fctEnergy = interp1(z,sumEnergies, sampling_pts, 'spline');
+force = diff(fctEnergy);
+
+%% Graphics
 figure(1);
+plot(z, x, 'r');
 
+figure(2);
+plot(z, sumEnergies, 'r'); hold on %plot sum of all Energies
+plot(sampling_pts(1:length(sampling_pts)-1), force, 'g'); %plot Forces
 
 %% FUNCTIONS
 function checkDims(L1, L2, L3, L4)
@@ -164,5 +215,14 @@ function [beta, EE_x, EE_z] = motionSim(alpha, L1, L2)
     EE_x = EE(1);
     EE_z = EE(2);
 end
-
+function energy = calcEnergy(p)
+    energy = 0.5*p.k*p.ener_var^2;
+end
+function stop = checkTolerances(Materials, Pivots, alpha, beta, z, x)
+    %parasitic motion tolerances
+    limit_trans = 1e-6;
+    limit_rot_xy = 50e-6;
+    limit_rot_z = 1e-6;
+    
+end
 
