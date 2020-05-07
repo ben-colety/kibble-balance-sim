a492a4a248964d88934ff939bf85a9f0635e8e0d
bcolety <61987823+ben-colety@users.noreply.github.com>
2020-05-07 21:09:00 +0200

    Update class_test.m

--- class_test.m
+++ class_test.m
@@ -10,36 +10,37 @@ b = 50e-3;
 
 pl2 = pivot('spring', 1, phi,1);
 pl4 = pivot('point', 1, phi);
+pl3 = pl2;
 
-Pivots = [pl2 pl4];
+Pivots = [pl2 pl3 pl4];
 
 
-for i = 1:2
-for j = 1
-switch Pivots(i).type
-    case 'spring'
-        syms h L
-        rig = Pivots(i).k == Pivots(i).num_lames * Materials(j).E * b * h^3 / L^3;
-        adm = max(abs(Pivots(i).ener_var)) == Materials(j).o_adm*L^2 /(3*Materials(j).E*h);
-        fprintf('lame for pivot %d\n',i)
-        Pivots(i) = pivotSolve(Pivots(i),rig, adm, h, L)
+for i = 1:length(Pivots)
+    for j = 1:length(Materials)
+        switch Pivots(i).type
+            case 'spring'
+                syms h L
+                rig = Pivots(i).k == Pivots(i).num_lames * Materials(j).E * b * h^3 / L^3;
+                adm = max(abs(Pivots(i).ener_var)) == Materials(j).o_adm*L^2 /(3*Materials(j).E*h);
+                fprintf('lame for pivot %d\n',i)
+                Pivots(i) = pivotSolve(Pivots(i),rig, adm, h, L)
 
-    case 'point'
-        %calculation as a col
-        syms e r
-        rig = Pivots(i).k == 2* Materials(j).E * b * e^(2.5) / (9*pi*r^(0.5));
-        adm = max(abs(Pivots(i).ener_var)) == 3*pi*Materials(j).o_adm*sqrt(r)/(4*Materials(j).E*sqrt(e));
-        fprintf('col for pivot %d\n',i)
-        Pivots(i) = pivotSolve(Pivots(i),rig, adm, e, r)
+            case 'point'
+                %calculation as a col
+                syms e r
+                rig = Pivots(i).k == 2* Materials(j).E * b * e^(2.5) / (9*pi*r^(0.5));
+                adm = max(abs(Pivots(i).ener_var)) == 3*pi*Materials(j).o_adm*sqrt(r)/(4*Materials(j).E*sqrt(e));
+                fprintf('col for pivot %d\n',i)
+                Pivots(i) = pivotSolve(Pivots(i),rig, adm, e, r)
 
-        %calculation as a cross
-        syms h L
-        rig2 = Pivots(i).k == 8*Materials(j).E*b*h^3 /(12*L);
-        adm2 = max(abs(Pivots(i).ener_var)) == Materials(j).o_adm * L /(2*Materials(j).E*h);
-        fprintf('cross for pivot %d\n',i)
-        Pivots(i) = pivotSolve(Pivots(i), rig2, adm2, h, L)
-end
-end
+                %calculation as a cross
+                syms h L
+                rig2 = Pivots(i).k == 8*Materials(j).E*b*h^3 /(12*L);
+                adm2 = max(abs(Pivots(i).ener_var)) == Materials(j).o_adm * L /(2*Materials(j).E*h);
+                fprintf('cross for pivot %d\n',i)
+                Pivots(i) = pivotSolve(Pivots(i), rig2, adm2, h, L)
+        end
+    end
 end
 
 % function [sol1 sol2] = pivotSolve(eq1, eq2, t, s)
